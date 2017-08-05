1a364f9227cffbdeeb59bcbc07919e0c4fcfe815
mrivera05 <mrivera05@manhattan.edu>
2017-08-05 15:49:54 -0400

    anything

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-console.log ('Vegetables are very healthy');
-alert ("Life is amazing");
-var vrHomePage = vRViewPlayer('#vr-homepage', {
-    image: 'mei.jpg',
-    is_stereo: false,
-    is_autopan_off: true,
+var myPicture = vRViewPlayer ('#skull-tattoo', {
+  image: 'picture1.jpg',
+  is_stereo : false
 });
+ window.addEventListener('load',myPicture);
+ myPicture.on('ready', function(event) {
+   myPicture.addHotspot('creating-hotspot-1', {
+     pitch: 0,
+     yaw: 0,
+     radius: 0.10,
+     distance: 1
+   });
+ });

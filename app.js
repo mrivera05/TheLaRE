7782fd672588f33951cf08386ef467503e0ff4c4
Marcia Sofia Rivera Castro <mrivera05@manhattan.edu>
2017-08-07 19:08:17 -0400

    Chanes HS5

--- app.js
+++ app.js
@@ -11,8 +11,26 @@ var myPicture = vRViewPlayer ('#skull-tattoo', {
      distance: 1
    });
    myPicture.addHotspot('creating-hotspot-2', {
-     pitch: 5,
+     pitch: 10,
      yaw: 0,
+     radius: 0.10,
+     distance: 1
+   });
+   myPicture.addHotspot('creating-hotspot-3', {
+     pitch: 0,
+     yaw: 10,
+     radius: 0.10,
+     distance: 1
+   });
+   myPicture.addHotspot('creating-hotspot-4', {
+     pitch: 5,
+     yaw: 15,
+     radius: 0.10,
+     distance: 1
+   });
+   myPicture.addHotspot('creating-hotspot-5', {
+     pitch: 5,
+     yaw: 10,
      radius: 0.10,
      distance: 1
    });
@@ -21,5 +39,21 @@ myPicture.on('click', function(event) {
   console.log ('register all click events');
   if (event.id == 'creating-hotspot-1') {
     alert('clicked Hotspot1!');
+  }
+  console.log ('register all click events');
+  if (event.id == 'creating-hotspot-2') {
+    alert('clicked Hotspot2!');
+  }
+  console.log ('register all click events');
+  if (event.id == 'creating-hotspot-3') {
+    alert('clicked Hotspot3!');
+  }
+  console.log ('register all click events');
+  if (event.id == 'creating-hotspot-4') {
+    alert('clicked Hotspot4!');
+  }
+  console.log ('register all click events');
+  alert('clicked Hotspot5!');
+  if (event.id == 'creating-hotspot-5') {
   }
 });

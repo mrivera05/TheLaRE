7302786802c3fb8ee8864f4cd878a8be0f3e22fa
Marcia Sofia Rivera Castro <mrivera05@manhattan.edu>
2017-08-12 14:23:44 -0400

    ff

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var myPicture = vRViewPlayer ('#skull-tattoo', {
    });
    myPicture.addHotspot('creating-hotspot-4', {
      pitch: 0,
-     yaw: 3000,
+     yaw: 1000,
      radius: 0.10,
      distance: 1
    });
